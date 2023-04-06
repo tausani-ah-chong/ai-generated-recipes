1521799f435782ab5e797ac927fb18ec469f6f1f
tausani93 <tausani93@gmail.com>
2023-04-06 12:38:27 +1200

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script 
+          async 
+          src="https://www.googletagmanager.com/gtag/js?id=G-MX66WX7KJV"
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MX66WX7KJV');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
